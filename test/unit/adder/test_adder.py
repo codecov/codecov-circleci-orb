5de9e0889a3e167e57e0190ccd091ceafd296258
Tom Hu <tomhu1096+gpg@gmail.com>
2021-07-15 14:43:44 -0700

    chore: Remove pytest for unittest

--- test/unit/adder/test_adder.py
+++ test/unit/adder/test_adder.py
@@ -1,8 +1,11 @@
-import pytest
+import unittest
+
 from src.adder.adder import Adder
 
-def test_sum_two_plus_two_is_four():
-    assert Adder().add(3,1) == 4
 
-def test_sum_two_plus_two_is_not_five():
-    assert Adder().add(3,4) != 6
+class TestAdderMethods(unittest.TestCase):
+    def test_sum_two_plus_two_is_four(self):
+        self.assertEqual(Adder().add(3, 1), 4)
+
+    def test_sum_two_plus_two_is_not_five(self):
+        self.assertNotEqual(Adder().add(3, 4), 6)
