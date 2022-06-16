8dbf999c816800ab7d494ccdda43fce5e8a3610b
Tom Hu <tom@codecov.io>
2022-06-16 11:25:53 -0400

    fix: alpine token

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -2,14 +2,16 @@ unset NODE_OPTIONS
 # See https://github.com/codecov/uploader/issues/475
 source $BASH_ENV
 chmod +x $filename
-echo "${!PARAM_TOKEN}"
+[[ $os == "alpine" ]] && token=${eval echo \$$PARAM_TOKEN}
+[[ $os != "alpine" ]] && token="${!PARAM_TOKEN}"
+echo $token
 [ -n "${PARAM_FILE}" ] && \
   set - "${@}" "-f" "${PARAM_FILE}"
 [ -n "${PARAM_XTRA_ARGS}" ] && \
   set - "${@}" "${PARAM_XTRA_ARGS}"
 ./"$filename" \
   -Q "codecov-circleci-orb-3.2.3" \
-  -t "${!PARAM_TOKEN}" \
+  -t "${token}" \
   -n "${PARAM_UPLOAD_NAME}" \
   -F "${PARAM_FLAGS}" \
   ${@}
