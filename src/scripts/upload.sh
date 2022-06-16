6f0cec2b40cedb03be2eaffff31dcdab86ed7813
Tom Hu <tom@codecov.io>
2022-06-16 11:03:38 -0400

    log: token

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -2,11 +2,12 @@ unset NODE_OPTIONS
 # See https://github.com/codecov/uploader/issues/475
 source $BASH_ENV
 chmod +x $filename
+echo "${PARAM_TOKEN}"
 [ -n "${PARAM_FILE}" ] && \
   set - "${@}" "-f" "${PARAM_FILE}"
 [ -n "${PARAM_XTRA_ARGS}" ] && \
   set - "${@}" "${PARAM_XTRA_ARGS}"
-./$filename \
+./"$filename" \
   -Q "codecov-circleci-orb-3.2.3" \
   -t "${PARAM_TOKEN}" \
   -n "${PARAM_UPLOAD_NAME}" \
