37f04d79455a7d22c5899c8bcc4371d0c061da21
Tom Hu <tom@codecov.io>
2022-06-16 11:35:22 -0400

    fix: use eval instead of indirect expansion

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -2,16 +2,13 @@ unset NODE_OPTIONS
 # See https://github.com/codecov/uploader/issues/475
 source $BASH_ENV
 chmod +x $filename
-[[ $os == "alpine" ]] && token=${eval echo \$$PARAM_TOKEN}
-[[ $os != "alpine" ]] && token="${!PARAM_TOKEN}"
-echo $token
 [ -n "${PARAM_FILE}" ] && \
   set - "${@}" "-f" "${PARAM_FILE}"
 [ -n "${PARAM_XTRA_ARGS}" ] && \
   set - "${@}" "${PARAM_XTRA_ARGS}"
 ./"$filename" \
   -Q "codecov-circleci-orb-3.2.3" \
-  -t "${token}" \
+  -t "${eval echo \$$PARAM_TOKEN}" \  # alpine doesn't allow for indirect expansion
   -n "${PARAM_UPLOAD_NAME}" \
   -F "${PARAM_FLAGS}" \
   ${@}
