80548dc62105500082166bee5a084d6d9d5d2175
Tom Hu <tom@codecov.io>
2023-03-15 21:39:10 -0400

    fix: prefix codecov to bash vars

--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -3,11 +3,11 @@ echo "${CODECOV_PUBLIC_PGP_KEY}" | \
   gpg --no-default-keyring --keyring trustedkeys.gpg --import
 # One-time step
 sha_url="https://uploader.codecov.io"
-sha_url="$sha_url/${version}/${os}"
-sha_url="$sha_url/${filename}.SHA256SUM"
+sha_url="$sha_url/${codecov_version}/${codecov_os}"
+sha_url="$sha_url/${codecov_filename}.SHA256SUM"
 echo "Downloading ${sha_url}"
 curl -Os $sha_url
 curl -Os $sha_url".sig"
-gpgv $filename.SHA256SUM.sig $filename.SHA256SUM
-shasum -a 256 -c $filename.SHA256SUM || \
-  sha256sum -c $filename.SHA256SUM
+gpgv $codecov_filename.SHA256SUM.sig $codecov_filename.SHA256SUM
+shasum -a 256 -c $codecov_filename.SHA256SUM || \
+  sha256sum -c $codecov_filename.SHA256SUM
