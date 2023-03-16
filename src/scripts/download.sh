b16ff9af0bfd39f22545ea2fc00251317af07c7b
Tom Hu <tom@codecov.io>
2023-03-15 21:39:10 -0400

    fix: prefix codecov to bash vars

--- src/scripts/download.sh
+++ src/scripts/download.sh
@@ -1,22 +1,22 @@
 family=$(uname -s | tr '[:upper:]' '[:lower:]')
-os="windows"
-[[ $family == "darwin" ]] && os="macos"
+codecov_os="windows"
+[[ $family == "darwin" ]] && codecov_os="macos"
 
-[[ $family == "linux" ]] && os="linux"
-[[ $os == "linux" ]] && \
+[[ $family == "linux" ]] && codecov_os="linux"
+[[ $codecov_os == "linux" ]] && \
   osID=$(grep -e "^ID=" /etc/os-release | cut -c4-)
-[[ $osID == "alpine" ]] && os="alpine"
-echo "Detected ${os}"
-echo "export os=${os}" >> $BASH_ENV
-echo "export version=${PARAM_VERSION}" >> $BASH_ENV
+[[ $osID == "alpine" ]] && codecov_os="alpine"
+echo "Detected ${codecov_os}"
+echo "export codecov_os=${codecov_os}" >> $BASH_ENV
+echo "export codecov_version=${PARAM_VERSION}" >> $BASH_ENV
 
-filename="codecov"
-[[ $os == "windows" ]] && filename+=".exe"
-echo "export filename=${filename}" >> $BASH_ENV
-[[ $os == "macos" ]] && \
+codecov_filename="codecov"
+[[ $codecov_os == "windows" ]] && codecov_filename+=".exe"
+echo "export codecov_filename=${codecov_filename}" >> $BASH_ENV
+[[ $codecov_os == "macos" ]] && \
   HOMEBREW_NO_AUTO_UPDATE=1 brew install gpg
 codecov_url="https://uploader.codecov.io"
 codecov_url="$codecov_url/${PARAM_VERSION}"
-codecov_url="$codecov_url/${os}/${filename}"
+codecov_url="$codecov_url/${codecov_os}/${codecov_filename}"
 echo "Downloading ${codecov_url}"
 curl -Os $codecov_url -v
--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -1,13 +1,13 @@
 unset NODE_OPTIONS
 # See https://github.com/codecov/uploader/issues/475
 source $BASH_ENV
-chmod +x $filename
+chmod +x $codecov_filename
 [ -n "${PARAM_FILE}" ] && \
   set - "${@}" "-f" "${PARAM_FILE}"
 [ -n "${PARAM_XTRA_ARGS}" ] && \
   set - "${@}" "${PARAM_XTRA_ARGS}"
 # alpine doesn't allow for indirect expansion
-./"$filename" \
+./"$codecov_filename" \
   -Q "codecov-circleci-orb-3.2.4" \
   -t "$(eval echo \$$PARAM_TOKEN)" \
   -n "${PARAM_UPLOAD_NAME}" \
