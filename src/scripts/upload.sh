ef95bb3d1677f01a2f3037a41697ed0b6f42b04f
joseph-sentry <joseph.sawaya@sentry.io>
2023-07-11 12:21:42 -0400

    Allow users to pass env var names as flags
    This commit allows users to specify an environment
    variable as a flag. The codecov uploader will
    use the contents of the environment variable as
    the flags when it runs.
    
    This commit checks if the string specified in
    PARAM_FLAGS corresponds to an env var and if
    it does it passes the contents to the flags option
    of the uploader. If it doesn't then it uses the
    PARAM_FLAGS string itself.
    
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -8,10 +8,25 @@ chmod +x $codecov_filename
   set - "${@}" "${PARAM_XTRA_ARGS}"
 [ -n "${PARAM_UPLOAD_NAME}" ] && \
   PARAM_UPLOAD_NAME="${CIRCLE_BUILD_NUM}"
+
+FLAGS=()
+IFS=',' read -ra FLAG_ARRAY <<< "$PARAM_FLAGS"
+for flag in "${FLAG_ARRAY[@]}"; do
+  eval e="\$$flag"
+  for param in "${e}" "${flag}"; do
+    if [ -n "${param}" ]; then
+      FLAGS+=("${param}")
+      break
+    fi
+  done
+done
+
+FINAL_FLAGS=$(IFS=',' ; echo "${FLAGS[*]}")
+
 # alpine doesn't allow for indirect expansion
 ./"$codecov_filename" \
   -Q "codecov-circleci-orb-3.2.5" \
   -t "$(eval echo \$$PARAM_TOKEN)" \
   -n "${PARAM_UPLOAD_NAME}" \
-  -F "${PARAM_FLAGS}" \
+  -F "${FINAL_FLAGS}" \
   ${@}
