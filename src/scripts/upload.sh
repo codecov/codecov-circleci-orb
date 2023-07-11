d04f47f4002f7f1d486624cc91a45ff394f3a190
joseph-sentry <joseph.sawaya@sentry.io>
2023-07-11 12:19:00 -0400

    Fix $CIRCLE_BUILD_NUM upload name bug
    This commit fixes the ${CIRCLE_BUILD_NAME} upload name bug
    by changing the upload_name parameter default string from
    "${CIRCLE_BUILD_NUM}" to the empty string, then modifying
    `upload.sh` to replace the PARAM_UPLOAD_NAME env var with
    the contents of CIRCLE_BUILD_NUM if PARAM_UPLOAD_NAME
    is the empty string.
    
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -6,6 +6,8 @@ chmod +x $codecov_filename
   set - "${@}" "-f" "${PARAM_FILE}"
 [ -n "${PARAM_XTRA_ARGS}" ] && \
   set - "${@}" "${PARAM_XTRA_ARGS}"
+[ -n "${PARAM_UPLOAD_NAME}" ] && \
+  PARAM_UPLOAD_NAME="${CIRCLE_BUILD_NUM}"
 # alpine doesn't allow for indirect expansion
 ./"$codecov_filename" \
   -Q "codecov-circleci-orb-3.2.5" \
