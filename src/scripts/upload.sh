4b0ff3ebdc20880ac5dd25f28e6c671fd7d92981
joseph-sentry <joseph.sawaya@sentry.io>
2023-07-20 17:31:22 -0400

    switch from using array to string for FLAGS
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -9,24 +9,27 @@ chmod +x $codecov_filename
 [ -n "${PARAM_UPLOAD_NAME}" ] && \
   PARAM_UPLOAD_NAME="${CIRCLE_BUILD_NUM}"
 
-FLAGS=()
-IFS=',' read -ra FLAG_ARRAY <<< "$PARAM_FLAGS"
-for flag in "${FLAG_ARRAY[@]}"; do
+FLAGS=""
+OLDIFS=$IFS;IFS=,
+for flag in $PARAM_FLAGS; do
   eval e="\$$flag"
   for param in "${e}" "${flag}"; do
     if [ -n "${param}" ]; then
-      FLAGS+=("${param}")
+      if [ -n "${FLAGS}" ]; then
+        FLAGS="${FLAGS},${param}"
+      else
+        FLAGS="${param}"
+      fi
       break
     fi
   done
 done
-
-FINAL_FLAGS=$(IFS=',' ; echo "${FLAGS[*]}")
+IFS=$OLDIFS
 
 # alpine doesn't allow for indirect expansion
 ./"$codecov_filename" \
   -Q "codecov-circleci-orb-3.2.5" \
   -t "$(eval echo \$$PARAM_TOKEN)" \
   -n "${PARAM_UPLOAD_NAME}" \
-  -F "${FINAL_FLAGS}" \
+  -F "${FLAGS}" \
   ${@}
