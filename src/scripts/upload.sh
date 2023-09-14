00fa42b0ef61d3302f0a4d2ea8436973c6ebade5
Tom Hu <thomas.hu@sentry.io>
2023-09-13 18:35:08 -0700

    fix: update args list and remove alpine

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -4,8 +4,8 @@ source $BASH_ENV
 chmod +x $codecov_filename
 [ -n "${PARAM_FILE}" ] && \
   set - "${@}" "-f" "${PARAM_FILE}"
-[ -n "${PARAM_XTRA_ARGS}" ] && \
-  set - "${@}" "${PARAM_XTRA_ARGS}"
+[ -n "${PARAM_UPLOAD_ARGS}" ] && \
+  set - "${@}" "${PARAM_UPLOAD_ARGS}"
 [ -n "${PARAM_UPLOAD_NAME}" ] && \
   PARAM_UPLOAD_NAME="${CIRCLE_BUILD_NUM}"
 
@@ -39,6 +39,7 @@ IFS=$OLDIFS
 #upload reports
 # alpine doesn't allow for indirect expansion
 ./"$codecov_filename" \
+  \$$PARAM_UPLOAD_ARGS \
   do-upload \
   -t "$(eval echo \$$PARAM_TOKEN)" \
   -n "${PARAM_UPLOAD_NAME}" \
