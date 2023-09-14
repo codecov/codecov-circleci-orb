521bc50fd60a3c7eee833a2a31930ae4dec11695
Tom Hu <thomas.hu@sentry.io>
2023-09-13 18:10:54 -0700

    fix: remove extra flag code

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -6,9 +6,26 @@ chmod +x $codecov_filename
   set - "${@}" "-f" "${PARAM_FILE}"
 [ -n "${PARAM_XTRA_ARGS}" ] && \
   set - "${@}" "${PARAM_XTRA_ARGS}"
-[ -n "${PARAM_FLAGS}" ] && \
-  set - "${@}" "-F" "${PARAM_FLAGS}"
-# alpine doesn't allow for indirect expansion
+[ -n "${PARAM_UPLOAD_NAME}" ] && \
+  PARAM_UPLOAD_NAME="${CIRCLE_BUILD_NUM}"
+
+FLAGS=""
+OLDIFS=$IFS;IFS=,
+for flag in $PARAM_FLAGS; do
+  eval e="\$$flag"
+  for param in "${e}" "${flag}"; do
+    if [ -n "${param}" ]; then
+      if [ -n "${FLAGS}" ]; then
+        FLAGS="${FLAGS},${param}"
+      else
+        FLAGS="${param}"
+      fi
+      break
+    fi
+  done
+done
+IFS=$OLDIFS
+
 #create commit
 ./"$filename" \
   create-commit \
@@ -20,6 +37,7 @@ chmod +x $codecov_filename
   -t "$(eval echo \$$PARAM_TOKEN)" \
 
 #upload reports
+# alpine doesn't allow for indirect expansion
 ./"$filename" \
   do-upload \
   -t "$(eval echo \$$PARAM_TOKEN)" \
