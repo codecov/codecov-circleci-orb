172c36da901dde9af440aea11d887c5a9672876c
Tom Hu <thomas.hu@sentry.io>
2023-09-25 22:21:36 -0700

    fix: update gpgv command to gpg --verify and bump to 3.3.0

--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -8,6 +8,6 @@ sha_url="$sha_url/${codecov_filename}.SHA256SUM"
 echo "Downloading ${sha_url}"
 curl -Os $sha_url
 curl -Os $sha_url".sig"
-gpgv $codecov_filename.SHA256SUM.sig $codecov_filename.SHA256SUM
+gpg --verify $codecov_filename.SHA256SUM.sig $codecov_filename.SHA256SUM
 shasum -a 256 -c $codecov_filename.SHA256SUM || \
   sha256sum -c $codecov_filename.SHA256SUM
