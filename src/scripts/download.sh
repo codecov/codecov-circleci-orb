48f1cb90e97b3ef13d0f06175ba48e3462a7ce14
Tom Hu <thomas.hu@sentry.io>
2023-10-26 23:58:01 +0800

    double quotes?

--- src/scripts/download.sh
+++ src/scripts/download.sh
@@ -1,4 +1,17 @@
-source $BASH_ENV
+family=$(uname -s | tr '[:upper:]' '[:lower:]')
+codecov_os="windows"
+[[ $family == "darwin" ]] && codecov_os="macos"
+
+[[ $family == "linux" ]] && codecov_os="linux"
+[[ $codecov_os == "linux" ]] && \
+  osID=$(grep -e "^ID=" /etc/os-release | cut -c4-)
+[[ $osID == "alpine" ]] && codecov_os="alpine"
+echo "Detected ${codecov_os}"
+echo "export codecov_os=${codecov_os}" >> $BASH_ENV
+
+codecov_filename="codecov"
+[[ $codecov_os == "windows" ]] && codecov_filename+=".exe"
+echo "export codecov_filename=${codecov_filename}" >> $BASH_ENV
 
 if [ -f ${codecov_filename} ]; then
   echo "${codecov_filename} already exists"
@@ -8,7 +21,7 @@ fi
 [[ $codecov_os == "macos" ]] && \
   HOMEBREW_NO_AUTO_UPDATE=1 brew install gpg
 codecov_url="https://uploader.codecov.io"
-codecov_url="$codecov_url/${codecov_version}"
+codecov_url="$codecov_url/${PARAM_VERSION}"
 codecov_url="$codecov_url/${codecov_os}/${codecov_filename}"
 echo "Downloading ${codecov_url}"
 curl -Os $codecov_url -v
