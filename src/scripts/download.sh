3de53e8e01f1b647aa1d43c8cbde19cab9d366c1
Tom Hu <thomas.hu@sentry.io>
2023-10-26 22:00:47 +0800

    fix: small edits

--- src/scripts/download.sh
+++ src/scripts/download.sh
@@ -8,7 +8,7 @@ fi
 [[ $codecov_os == "macos" ]] && \
   HOMEBREW_NO_AUTO_UPDATE=1 brew install gpg
 codecov_url="https://uploader.codecov.io"
-codecov_url="$codecov_url/${PARAM_VERSION}"
+codecov_url="$codecov_url/${codecov_version}}"
 codecov_url="$codecov_url/${codecov_os}/${codecov_filename}"
 echo "Downloading ${codecov_url}"
 curl -Os $codecov_url -v
