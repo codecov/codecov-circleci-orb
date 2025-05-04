199f3d22393c26066ac3ab47df5fdbaf90b7ec9d
Tom Hu <thomas.hu@sentry.io>
2025-05-04 16:56:22 -0700

    fix: add set codecov envs

---
+++ src/dist/set_codecov_envs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+touch ./codecov_envs
+
+export CODECOV_BINARY_LOCATION=
+export CODECOV_CLI_URL=
+export CODECOV_DOWNLOAD_ONLY=
+export CODECOV_GCOV_ARGS=
+export CODECOV_GCOV_EXECUTABLE=
+export CODECOV_GCOV_IGNORE=
+export CODECOV_GCOV_INCLUDE=
+export CODECOV_PUBLIC_PGP_KEY=
+export CODECOV_SWIFT_PROJECT=
+export CODECOV_TOKEN=
+export CODECOV_WRAPPER_VERSION=
+export CODECOV_YML_PATH=
+
+env | grep "CODECOV_" | tee codecov_envs
