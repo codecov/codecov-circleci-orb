df8b9d58547e9fa42e5b2cf2cd146b98130d91ca
Tom Hu <thomas.hu@sentry.io>
2025-05-04 11:04:48 -0700

    fix: try out env file

---
+++ src/dist/set_defaults.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+source ./codecov_envs
+set +u
+say() {
+  echo -e "$1"
+}
+
+exit_if_error() {
+  say "$r==> $1$x"
+  if [ "$CODECOV_FAIL_ON_ERROR" = true ];
+  then
+     say "$r    Exiting...$x"
+     exit 1;
+  fi
+}
+
+lower() {
+  echo $(echo $1 | sed 's/CODECOV//' | sed 's/_/-/g' | tr '[:upper:]' '[:lower:]')
+}
+
+k_arg() {
+  if [ -n "$(eval echo \$"CODECOV_$1")" ];
+  then
+    echo "--$(lower "$1")"
+  fi
+}
+
+v_arg() {
+  if [ -n "$(eval echo \$"CODECOV_$1")" ];
+  then
+    echo "$(eval echo \$"CODECOV_$1")"
+  fi
+}
+
+write_bool_args() {
+  if [ "$(eval echo \$$1)" = "true" ] || [ "$(eval echo \$$1)" = "1" ];
+  then
+    echo "-$(lower $1)"
+  fi
+}
+
+b="\033[0;36m"  # variables/constants
+g="\033[0;32m"  # info/debug
+r="\033[0;31m"  # errors
+x="\033[0m"
+retry="--retry 5 --retry-delay 2"
+
+say "     _____          _
+    / ____|        | |
+   | |     ___   __| | ___  ___ _____   __
+   | |    / _ \\ / _\` |/ _ \\/ __/ _ \\ \\ / /
+   | |___| (_) | (_| |  __/ (_| (_) \\ V /
+    \\_____\\___/ \\__,_|\\___|\\___\\___/ \\_/
+                           $r Wrapper-$CODECOV_WRAPPER_VERSION$x
+                                  "
+
+CODECOV_VERSION="${CODECOV_VERSION:-latest}"
+CODECOV_FAIL_ON_ERROR="${CODECOV_FAIL_ON_ERROR:-false}"
+CODECOV_RUN_CMD="${CODECOV_RUN_CMD:-upload-coverage}"
+env | grep "CODECOV_" > ./codecov_envs
