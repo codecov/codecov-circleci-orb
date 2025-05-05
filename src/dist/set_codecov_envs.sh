e6b19ffd9154346888496459e806f0f000a936dd
Tom Hu <thomas.hu@sentry.io>
2025-05-04 17:02:15 -0700

    fix: more things

--- src/dist/set_codecov_envs.sh
+++ src/dist/set_codecov_envs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 touch ./codecov_envs
+chmod u+x ./codecov_envs
+"#!/usr/bin/env bash" > ./codecov_envs
 
 export CODECOV_BINARY_LOCATION=
 export CODECOV_CLI_URL=
@@ -15,4 +17,4 @@ export CODECOV_TOKEN=
 export CODECOV_WRAPPER_VERSION=
 export CODECOV_YML_PATH=
 
-env | grep "CODECOV_" | tee codecov_envs
+env | grep "CODECOV_" | tee ./codecov_envs
