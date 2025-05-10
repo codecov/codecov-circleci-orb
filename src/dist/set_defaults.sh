a41eadafe1a93655045d9fb5be80dd8b4cdca0c0
Tom Hu <thomas.hu@sentry.io>
2025-05-09 18:30:22 -0700

    fix: no quotes

--- src/dist/set_defaults.sh
+++ src/dist/set_defaults.sh
@@ -59,7 +59,5 @@ say "     _____          _
     \\_____\\___/ \\__,_|\\___|\\___\\___/ \\_/
                            $r Wrapper-$CODECOV_WRAPPER_VERSION$x
                            "
-env | grep -io "CODECOV_.*=" | tr "=" " " | while read -r val; do echo "export $val=\"$(eval echo \"\$$val\")\""; done; > ./codecov_envs
-echo "its here"
-ls -alrt
+env | grep -io "CODECOV_.*=" | tr "=" " " | while read -r val; do echo "export $val=$(eval echo \"\$$val\")"; done; > ./codecov_envs
 cat ./codecov_envs
