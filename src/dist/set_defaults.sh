e8b44fc2964d1425037c7605a06923ff083c9d4d
Tom Hu <thomas.hu@sentry.io>
2025-05-09 18:16:00 -0700

    fix: cat it

--- src/dist/set_defaults.sh
+++ src/dist/set_defaults.sh
@@ -60,3 +60,4 @@ say "     _____          _
                            $r Wrapper-$CODECOV_WRAPPER_VERSION$x
                            "
 env | grep -io "CODECOV_.*=" | tr "=" " " | while read -r val; do echo "export $val=\"$(eval echo \"\$$val\")\""; done; > ./codecov_envs
+cat ./codecov_envs
--- src/dist/validate.sh
+++ src/dist/validate.sh
@@ -91,3 +91,4 @@ then
   say "$g==>$x Codecov download only called. Exiting..."
 fi
 env | grep -io "CODECOV_.*=" | tr "=" " " | while read -r val; do echo "export $val=\"$(eval echo \"\$$val\")\""; done; > ./codecov_envs
+cat ./codecov_envs
