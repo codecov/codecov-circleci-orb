3f9be03e290e2b0e9e13b125250d761a10e22429
Tom Hu <thomas.hu@sentry.io>
2025-05-10 17:29:31 -0700

    fix: cleanup package.py

--- src/package.py
+++ src/package.py
@@ -64,26 +64,5 @@ def _copy_all_files(funcs):
 
             print(f'Copied {old_file} to {new_file} ({len(contents)} chars)')
 
-
-def _filter_row(row):
-    exclude_rows = set([
-        'CC_AUTO_LOAD_PARAMS_FROM',
-        'CC_BINARY_LOCATION',
-        'CC_CLI_URL',
-        'CC_DOWNLOAD_ONLY',
-        'CC_GCOV_ARGS',
-        'CC_GCOV_EXECUTABLE',
-        'CC_GCOV_IGNORE',
-        'CC_GCOV_INCLUDE',
-        'CC_PUBLIC_PGP_KEY',
-        'CC_SWIFT_PROJECT',
-    ])
-
-    row = row.strip()
-    if row in exclude_rows:
-        return None
-    return row
-
-
 if __name__=="__main__":
     package()
